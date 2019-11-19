0d63d9462160eb836e24c90c2d05121051e8fc06
wangyao-april <55935813+wangyao-april@users.noreply.github.com>
2019-11-19 20:58:40 +0800

    项目

---
+++ star/pages/index/index.js
@@ -0,0 +1,110 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    banners:[
+      '/assets/boy.jpg',
+      '/assets/1.jpg',
+      '/assets/2.jpg',
+      '/assets/boy.jpg',
+    ],
+    list:[{
+      id:1,
+      title:'圣诞奇妙味',
+      image:'/assets/boy.jpg'
+    },
+    {
+      id: 2,
+      title: 'DIY圣诞心意',
+      image: '/assets/boy.jpg'
+    },
+    {
+      id: 3,
+      title: '为努力喝彩',
+      image: '/assets/boy.jpg'
+    },
+    {
+      id: 4,
+      title: '感恩有你',
+      image: '/assets/boy.jpg'
+    },
+    {
+      id: 5,
+      title: '心动传情',
+      image: '/assets/boy.jpg'
+    },
+    {
+      id: 6,
+      title: '可爱第一名',
+      image: '/assets/boy.jpg'
+    },
+    {
+      id: 7,
+      title: '咖啡有你',
+      image: '/assets/boy.jpg'
+    },
+    {
+      id: 8,
+      title: '生日快乐',
+      image: '/assets/boy.jpg'
+    }]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
